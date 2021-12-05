2d604e60b7866648f2a3434ab95ac5d90ee17843
kalaiarasi0524 <kalaiarasi0524@gmail.com>
2021-12-05 14:21:18 -0500

    Added and modified files

--- src/BudgetHome.java
+++ src/BudgetHome.java
@@ -3,7 +3,12 @@ public class BudgetHome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
+		TestOOPSConcept testOOPS =new TestOOPSConcept();
+		testOOPS.ShowWelcomeMessage("Rajeshkumar Govindaraj");
+		testOOPS.ShowWelcomeMessage("Rajeshkumar","Govindaraj");
+		testOOPS.ShowWelcomeMessage("Rajeshkumar","T","Govindaraj");
 	}
 
+	
+	
 }
---
+++ src/TestOOPSConcept.java
@@ -0,0 +1,19 @@
+
+public class TestOOPSConcept {
+	public void ShowWelcomeMessage(String userName) {
+		System.out.println("Calling first Method");
+		System.out.println("Hello" + userName);
+	}
+	
+	public void ShowWelcomeMessage(String firstName, String lastName) {
+		System.out.println("Calling second Method");
+		String userName = firstName + " " + lastName;
+		System.out.println("Hello" + userName);	
+	}
+	
+	public void ShowWelcomeMessage(String firstName,String middleName, String lastName) {
+		System.out.println("Calling third Method");
+		String userName = firstName + " " +middleName+" "+ lastName;
+		System.out.println("Hello" + userName);	
+	}
+}
